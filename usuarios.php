7fcd3a4e2637160e8f486ebca1ce1d71696d1d2d
milton felga <milton.felga@etec.sp.gov.br>
2019-08-29 22:19:53 -0300

    adicionando campos novo usuario

--- usuarios.php
+++ usuarios.php
@@ -89,11 +89,30 @@
 						</tr>
 					</tbody>
 				</table>
-
+				<form>
+					<div class="form-group">
+						<div class="form-group">
+							<label for="exampleInputPassword1">Nome</label>
+							<input type="name" class="form-control" id="exampleInputName1" placeholder="Nome">
+						</div>
+						<label for="exampleInputEmail1">Endereço de email</label>
+						<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email">
+						<small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Senha</label>
+						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+					</div>
+					<div class="form-group form-check">
+						<input type="checkbox" class="form-check-input" id="exampleCheck1">
+						<label class="form-check-label" for="exampleCheck1">Clique em mim</label>
+					</div>
+					<button type="submit" class="btn btn-primary">Enviar</button>
+				</form>
 			</div>
-
 		</div>
 	</div>
+
 
 
 </body>
