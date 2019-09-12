8619841ea57f1b2fe244507707d9ff2bd6346529
milton felga <milton.felga@etec.sp.gov.br>
2019-09-11 22:13:17 -0300

    adicionado header

--- UsuarioDAO.php
+++ UsuarioDAO.php
@@ -10,7 +10,7 @@ class UsuarioDAO{
 		$sql = "INSERT INTO usuarios VALUES (0,'$this->nome','$this->email','$this->senha')";
 		$rs = $con ->query($sql);
 		if($rs)
-			echo "usuário inserido com sucesso";
+			header ("Location:usuarios.php");
 		else 
 			echo $con->error;
 	}
